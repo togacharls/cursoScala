b2292ac3985b5a2a08256a9c1c4f99855fd62bec
Sergio Couto <ingscc00@gmail.com>
2017-10-15 18:27:16 +0200

    sesion 1 updated

--- code/src/main/scala/com/github/scouto/intro/Intro.scala
+++ code/src/main/scala/com/github/scouto/intro/Intro.scala
@@ -4,16 +4,29 @@ package com.github.scouto.intro
   * Created by scouto.
   */
 object MyApp extends App {
-  println("Hello World!")
+  println("Hello World ")
 
   def sum(x: Int, y: Int): Int = {
     x + y
   }
 
+
+
+
+
+
   def wrongFactorial(n: Int): Int = {
-    if (n <= 0) n
+    if (n <= 0) 1
     else n * factorial(n - 1)
   }
+
+
+
+
+
+
+
+
 
 
   def factorial(n: Int): Int = {
