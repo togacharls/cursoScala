6290d8853a3638774a374f8785fc8f921a8f3fc6
Sergio Couto <ingscc00@gmail.com>
2017-10-15 11:34:09 +0200

    sesion 1

---
+++ code/src/test/scala/com/github/scouto/intro/IntroTest.scala
@@ -0,0 +1,23 @@
+package com.github.scouto.intro
+
+/**
+  * Created by scouto.
+  */
+
+import com.github.scouto.intro.MyApp._
+import org.scalatest._
+
+class IntroTest extends FlatSpec with Matchers {
+
+
+
+  "sum" should "work with both natural numbers" in {
+    sum(2,3) should be (5)
+  }
+
+  "factorial" should "be 1 for any integer below 0" in {
+    factorial(-5) should be (1)
+    factorial(-1) should be (1)
+  }
+
+}
