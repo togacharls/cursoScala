97fdae0970f31fc223034bc87dfe3f6b823fbf06
Sergio Couto <ingscc00@gmail.com>
2017-10-28 11:34:31 +0200

    sesion6

--- code/src/main/scala/com/github/scouto/sesion5/Lista.scala
+++ code/src/main/scala/com/github/scouto/sesion5/Lista.scala
@@ -15,9 +15,15 @@ object Lista {
     if (as.isEmpty) Vacio
     else Cons(as.head, apply(as.tail: _*))
 
-  def sum(ints: Lista[Int]): Int = ???
+  def sum(ints: Lista[Int]): Int = ints match {
+    case Vacio => 0
+    case Cons(x, xs) => x + sum(xs)
+  }
 
-  def product(ints: Lista[Int]) : Double = ???
+  def product(ints: Lista[Int]) : Double = ints match {
+    case Vacio => 1.0
+    case Cons(x, xs) => x * product(xs)
+  }
 
   def tail[A](list: Lista[A]): Lista[A] = ???
 
@@ -37,6 +43,37 @@ object Lista {
 
 
   def init[A](l: Lista[A]): Lista[A] = ???
+
+  // -------------------- Sesion 6 --------------------
+
+  def foldRight[A,B](as: Lista[A], z: B) (f: (A, B) => B) : B = ???
+
+  def sumFold(ints: Lista[Int]): Int = ???
+
+  def productFold(ints: Lista[Int]) : Double = ???
+
+  def length[A](as: Lista[A]): Int = ???
+
+  //@tailrec
+  def foldLeft[A,B](as: Lista[A], z: B) (f: (B, A) => B) : B = ???
+
+  def sumFoldLeft(ints: Lista[Int]): Int = ???
+
+  def productFoldLeft(ints: Lista[Int]) : Double = ???
+
+  def lengthFoldLeft[A](as: Lista[A]): Int = ???
+
+  def reverse[A](as: Lista[A]): Lista [A] = ???
+
+  def foldRightbyLeft[A,B](as: Lista[A], z: B) (f: (B, A) => B) : B = ???
+
+  def foldLeftbyRight[A,B](as: Lista[A], z: B) (f: (B, A) => B) : B = ???
+
+  def appendfoldLeft[A](a1: Lista[A], a2: Lista[A]): Lista[A] = ???
+
+  def appendfoldLeft[A](a1: Lista[A], a2: Lista[A]): Lista[A] = ???
+
+  def appendLists[A](as: List[List[A]]): List[A] = ???
 
   }
 
